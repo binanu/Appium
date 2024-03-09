9fb1f6c772e5958eb6339ffdff5d947a7c2f5bad
Bini <findbiniyam@gmail.com>
2024-03-08 23:34:07 -0500

    Add another main method

--- BasicTestClass.java
+++ BasicTestClass.java
@@ -7,20 +7,23 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class BasicTestClass extends BaseClass {
+public class BasicTestClass { //extends BaseClass 
 
-	public static AndroidDriver<AndroidElement> main(String[] args) throws MalformedURLException {
-		
-		File file = new File("src/main/resources");
-		File fs = new File(file,"ApiDemos-debug.apk");
-		DesiredCapabilities cap = new DesiredCapabilities();
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "MyPixelXLEmulator");
-		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
-		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+//	public static AndroidDriver<AndroidElement> main(String[] args) throws MalformedURLException {
+//		
+//		File file = new File("src/main/resources");
+//		File fs = new File(file,"ApiDemos-debug.apk");
+//		DesiredCapabilities cap = new DesiredCapabilities();
+//		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "MyPixelXLEmulator");
+//		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+//		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+//		
+//		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement> (new URL("http://127.0.0.1:4723/wd/hub"),cap);
+//		return driver;
+//		
+//	}
+	public static void main(String[] args) {
 		System.out.println("You are awesome!!!!");
-		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement> (new URL("http://127.0.0.1:4723/wd/hub"),cap);
-		return driver;
-		
 	}
 
 }
